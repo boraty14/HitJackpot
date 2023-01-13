103a7c1fa1337bb030c98de2ffd80a2ec1a2c5c3
braty <brataysen@gmail.com>
2023-01-13 04:13:28 +0300

    coin effect and pooling added

--- Assets/Game/Scripts/SlotController.cs
+++ Assets/Game/Scripts/SlotController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Lean.Pool;
 using UnityEngine;
 
 public class SlotController : MonoBehaviour
@@ -8,6 +9,8 @@ public class SlotController : MonoBehaviour
     [SerializeField] private List<Slot> _slots;
     [SerializeField] private SpinData _spinData;
     [SerializeField] private SpinSettings _spinSettings;
+    [SerializeField] private CoinEffect _coinEffectPrefab;
+    [SerializeField] private Transform _coinEffectTransform;
     public Action<bool> OnSpinStateChange;
     private bool _isSpinning;
     
@@ -22,31 +25,46 @@ public class SlotController : MonoBehaviour
     public async void Spin(int spinIndex)
     {
         if (_isSpinning) return;
+        StartSpinning();
 
-        _isSpinning = true;
-        OnSpinStateChange?.Invoke(_isSpinning);
         var spinResult = await SpinDefault();
-
         var spinSlowDownDuration = GetSpinSlowDownDuration(spinIndex);
+        var coinEffectRate = GetCoinEffectEmissionRate(spinResult);
         if (spinSlowDownDuration < 0f)
         {
-            _isSpinning = false;
-            OnSpinStateChange?.Invoke(_isSpinning);
+            StopSpinning(coinEffectRate);
             return;
         }
         
         await _slots[2].SpinDelayedSlotToState(spinResult.thirdSpin, spinSlowDownDuration);
+        StopSpinning(coinEffectRate);
+    }
+
+    private void StartSpinning()
+    {
+        _isSpinning = true;
+        OnSpinStateChange?.Invoke(_isSpinning);
+        
+    }
+
+    private void StopSpinning(float coinEffectRate)
+    {
         _isSpinning = false;
         OnSpinStateChange?.Invoke(_isSpinning);
+
+        if (coinEffectRate < 0.1f) return;
+        var coinEffect = LeanPool.Spawn(_coinEffectPrefab,_coinEffectTransform);
+        coinEffect.SetParticleEmission(coinEffectRate);
+
     }
 
     private async Task<SpinResult> SpinDefault()
     {
         var spinResult = _spinData.Spin();
         Debug.Log(spinResult.firstSpin + " " + spinResult.secondSpin + " " + spinResult.thirdSpin);
-        _ = _slots[0].SpinDefaultSlotToState(spinResult.firstSpin,_spinSettings.DefaultSpintTurnCount + 0);
-        _ = _slots[1].SpinDefaultSlotToState(spinResult.secondSpin,_spinSettings.DefaultSpintTurnCount + 1);
-        await _slots[2].SpinDefaultSlotToState(spinResult.thirdSpin,_spinSettings.DefaultSpintTurnCount + 2);
+        _ = _slots[0].SpinDefaultSlotToState(spinResult.firstSpin,_spinSettings.DefaultSpinTurnCount + 0);
+        _ = _slots[1].SpinDefaultSlotToState(spinResult.secondSpin,_spinSettings.DefaultSpinTurnCount + 1);
+        await _slots[2].SpinDefaultSlotToState(spinResult.thirdSpin,_spinSettings.DefaultSpinTurnCount + 2);
         return spinResult;
     }
 
@@ -56,9 +74,27 @@ public class SlotController : MonoBehaviour
         {
             0 => -1f,
             1 => _spinSettings.NormalSpinSlowDownDuration,
-            2 => _spinSettings.FastSpinItemPassDuration,
+            2 => _spinSettings.SlowSpinSlowDownDuration,
             _ => -1f
         };
+    }
+
+    private float GetCoinEffectEmissionRate(SpinResult spinResult)
+    {
+        if (spinResult.firstSpin == spinResult.secondSpin && spinResult.secondSpin == spinResult.thirdSpin)
+        {
+            return spinResult.firstSpin switch
+            {
+                SpinType.Jackpot => 5f * _spinSettings.CoinEffectRate,
+                SpinType.Wild => 4f * _spinSettings.CoinEffectRate,
+                SpinType.Seven => 3f * _spinSettings.CoinEffectRate,
+                SpinType.Bonus => 2f * _spinSettings.CoinEffectRate,
+                SpinType.A => _spinSettings.CoinEffectRate,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        return 0f;
     }
     
 }
--- Assets/Game/Scripts/SpinSettings.cs
+++ Assets/Game/Scripts/SpinSettings.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class SpinSettings : ScriptableObject
 {
     [field: SerializeField] public float FastSpinItemPassDuration { get; private set; }
-    [field: SerializeField] public int DefaultSpintTurnCount { get; private set; }
+    [field: SerializeField] public int DefaultSpinTurnCount { get; private set; }
     [field: SerializeField] public float NormalSpinSlowDownDuration { get; private set; }
     [field: SerializeField] public float SlowSpinSlowDownDuration { get; private set; }
+    [field: SerializeField] public float CoinEffectRate { get; private set; }
+    [field: SerializeField] public float CoinEffectDuration { get; private set; }
 }
