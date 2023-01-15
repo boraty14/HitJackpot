aa6f3525675d47f02febbe39628694306f69e4ef
braty <brataysen@gmail.com>
2023-01-16 00:01:39 +0300

    code refactor and comments

--- Assets/Game/Scripts/GameElements/SpinGenerator.cs
+++ Assets/Game/Scripts/GameElements/SpinGenerator.cs
@@ -12,37 +12,39 @@ namespace Game.Scripts.GameElements
         public SpinResultList spinResultList;
         public List<SpinData> spinDataList;
         [HideInInspector] public int spinIndex;
+        public Dictionary<SpinData, int> RemainExtensionCountDictionary => _remainExtensionCountDictionary;
+        private Dictionary<SpinData, int> _resultAppearCountDictionary;
+        private Dictionary<SpinData, int> _remainExtensionCountDictionary;
+        private Dictionary<SpinData, int> _startIndexDictionary;
         private const string SaveKey = "SAVEKEY";
-        public Dictionary<SpinData, int> RemainExtensionCountDictionary { get; private set; }
-    
+
         public void GenerateSpinListNew()
         {
-            ES3.DeleteKey(SaveKey);
-            spinIndex = 0;
+            ResetStates();
             spinResultList.Value = new SpinResult[100];
             bool[] resultOccupiedArray = new bool[100];
-            Dictionary<SpinData, int> resultAppearCountDictionary = new Dictionary<SpinData, int>();
+            var sortedSpinDataList = spinDataList.OrderByDescending(result => result.percentage).ToList();
+            
+            //set dictionaries
+            _resultAppearCountDictionary = new Dictionary<SpinData, int>();
             //remain ext dictionary is for selecting the intervals more precisely.
             //For example if we have a %13 probability, 9 interval will be 8 element long and 4 interval
             // will be 7 element long. This dictionary holds how many times a spin result is increased its
             // interval by 1.
-            Dictionary<SpinData, int> remainExtensionCountDictionary = new Dictionary<SpinData, int>();
-            Dictionary<SpinData, int> startIndexDictionary = new Dictionary<SpinData, int>();
-        
-            var sortedResults = spinDataList.OrderByDescending(result => result.percentage).ToList();
-            //set dictionaries
+            _remainExtensionCountDictionary = new Dictionary<SpinData, int>();
+            _startIndexDictionary = new Dictionary<SpinData, int>();
             foreach (var result in spinDataList)
-            { 
-                resultAppearCountDictionary[result] = 0;
-                startIndexDictionary[result] = 0;
-                remainExtensionCountDictionary[result] = 0;
+            {
+                _resultAppearCountDictionary[result] = 0;
+                _startIndexDictionary[result] = 0;
+                _remainExtensionCountDictionary[result] = 0;
             }
 
-        
+
             //set 100 spin
             for (int i = 0; i < 100; i++)
             {
-                // so basically this part is for selecting spin with the at least interval limit.
+                // so basically this part is for selecting spin with the minimum interval limit.
                 // According to their appearance count and last interval start index, which both
                 // hold in dictionaries, I select the interval with minimal upper limit. For example
                 // we can have possibilities that should appear between 17-33, 20-39,24-29 ...
@@ -52,56 +54,42 @@ namespace Game.Scripts.GameElements
                 // The point is for probabilities that have remainder after dividing 100, not all the intervals are same.
                 // By checking if spin interval should be extended according to extension count and empty indices, we extend
                 // the interval or not. 
-            
+
                 //get minimal interval limit
-                var currentResult = sortedResults[0];
-                for (int j = 1; j < sortedResults.Count; j++)
+                var currentSpinData = sortedSpinDataList[0];
+                for (int j = 1; j < sortedSpinDataList.Count; j++)
                 {
-                    var comparingResult = sortedResults[j];
-                    int comparingResultInterval = 100 / comparingResult.percentage;
-                    int comparingResultRemainFactor = (100 % comparingResult.percentage != 0) &&
-                                                      remainExtensionCountDictionary[comparingResult] < (100 % comparingResult.percentage) ?
-                        1:0;
+                    var comparingSpinData = sortedSpinDataList[j];
+                    int comparingSpinDataLimit = GetCurrentIntervalLimit(comparingSpinData);
+                    int currentSpinDataLimit = GetCurrentIntervalLimit(currentSpinData);
 
-                    int currentResultInterval = 100 / currentResult.percentage;
-                    int currentResultRemainFactor = (100 % currentResult.percentage != 0) &&
-                                                    remainExtensionCountDictionary[currentResult] <
-                                                    (100 % currentResult.percentage)
-                        ? 1 : 0;
-                
-                    int comparingResultLimit = startIndexDictionary[comparingResult] + comparingResultInterval +
-                                               comparingResultRemainFactor;
-
-                    int currentResultLimit = startIndexDictionary[currentResult] + currentResultInterval +
-                                             currentResultRemainFactor;
-                
-
-                    if (comparingResultLimit == currentResultLimit)
+                    if (comparingSpinDataLimit == currentSpinDataLimit)
                     {
-                        if (startIndexDictionary[comparingResult] < startIndexDictionary[currentResult])
+                        if (_startIndexDictionary[comparingSpinData] < _startIndexDictionary[currentSpinData])
                         {
-                            currentResult = comparingResult;
+                            currentSpinData = comparingSpinData;
                         }
 
                         //add randomness for the same weighted probabilities
-                        else if(comparingResult.percentage == currentResult.percentage)
+                        else if (comparingSpinData.percentage == currentSpinData.percentage)
                         {
                             int randomSelectIndex = Random.Range(0, 2);
-                            if (randomSelectIndex == 1) currentResult = comparingResult;
+                            if (randomSelectIndex == 1) currentSpinData = comparingSpinData;
                         }
-                    
                     }
-                
-                    else if (comparingResultLimit < currentResultLimit) currentResult = comparingResult;
+
+                    else if (comparingSpinDataLimit < currentSpinDataLimit) currentSpinData = comparingSpinData;
                 }
 
                 // after getting result set its index in the first available space in interval
                 // and check if we should and if we will extend the interval
-                int resultInterval = 100 / currentResult.percentage;
-                int resultRemainFactor = (100 % currentResult.percentage != 0) &&
-                                         remainExtensionCountDictionary[currentResult] < (100 % currentResult.percentage) ?
-                    1:0;
-                int placementIndex = startIndexDictionary[currentResult];
+                int resultInterval = 100 / currentSpinData.percentage;
+                int resultRemainFactor = (100 % currentSpinData.percentage != 0) &&
+                                         _remainExtensionCountDictionary[currentSpinData] <
+                                         (100 % currentSpinData.percentage)
+                    ? 1
+                    : 0;
+                int placementIndex = _startIndexDictionary[currentSpinData];
                 int placementIndexOffset = 0;
                 while (resultOccupiedArray[placementIndex + placementIndexOffset])
                 {
@@ -109,36 +97,49 @@ namespace Game.Scripts.GameElements
                     if (placementIndex + placementIndexOffset >= 100)
                     {
                         // SHOULD NOT REACH HERE BUT JUST IN CASE IF I MADE A MISTAKE
-                        Debug.LogError("placement index " + placementIndex + " percentage " + currentResult.percentage); 
+                        Debug.LogError("placement index " + placementIndex + " percentage " + currentSpinData.percentage);
                         return;
                     }
                 }
 
                 // set the result and update dictionaries
                 resultOccupiedArray[placementIndex + placementIndexOffset] = true;
-                startIndexDictionary[currentResult] += resultInterval;
+                _startIndexDictionary[currentSpinData] += resultInterval;
+                
+                // what I check here is if I had to extend interval to find an available index for placement, or
+                // if the other intervals were not extended and from this point I have to extend this interval no matter
+                // what to fill all the interval up to index 99. If it is the case, extend the interval
                 if ((placementIndexOffset >= resultInterval + resultRemainFactor - 1 && resultRemainFactor == 1) ||
-                    ((100 % currentResult.percentage) - remainExtensionCountDictionary[currentResult]) ==
-                    (currentResult.percentage) - resultAppearCountDictionary[currentResult] && 
-                    100 % currentResult.percentage != 0)
+                    ((100 % currentSpinData.percentage) - _remainExtensionCountDictionary[currentSpinData]) ==
+                    (currentSpinData.percentage) - _resultAppearCountDictionary[currentSpinData] &&
+                    100 % currentSpinData.percentage != 0)
                 {
-                    startIndexDictionary[currentResult]++;
-                    remainExtensionCountDictionary[currentResult]++;
+                    _startIndexDictionary[currentSpinData]++;
+                    _remainExtensionCountDictionary[currentSpinData]++;
                 }
-                resultAppearCountDictionary[currentResult]++;
-                spinResultList.Value[placementIndex + placementIndexOffset] = currentResult.spinResult;
 
+                _resultAppearCountDictionary[currentSpinData]++;
+                spinResultList.Value[placementIndex + placementIndexOffset] = currentSpinData.spinResult;
             }
 
-            RemainExtensionCountDictionary = remainExtensionCountDictionary;
-        
-        
             // Note: In general code should not contain this much comment and explain itself and a bit cleaner maybe.
             // But this function in the whole project represents the algorithm used for probability distribution and
             // it needs explaining. Optimization can be made but since the system itself does not trigger on the
             // gameplay spinning and sets before game starts, I think it is acceptable. Also, other than
             // this function this project does not have any other comments. I believe rest of the project
             // is self explanatory.
+        }
+
+        private int GetCurrentIntervalLimit(SpinData spinData)
+        {
+            int spinInterval = 100 / spinData.percentage;
+            int spinRemainFactor = (100 % spinData.percentage != 0) &&
+                                   _remainExtensionCountDictionary[spinData] <
+                                   (100 % spinData.percentage)
+                ? 1
+                : 0;
+            
+            return _startIndexDictionary[spinData] + spinInterval + spinRemainFactor;
         }
 
         public SpinResult Spin()
@@ -162,7 +163,7 @@ namespace Game.Scripts.GameElements
                 spinResults = spinResultList.Value,
                 spinIndex = this.spinIndex
             };
-            ES3.Save<SpinSave>(SaveKey,spinSave);
+            ES3.Save<SpinSave>(SaveKey, spinSave);
         }
 
         public void LoadSpinData()
@@ -175,17 +176,17 @@ namespace Game.Scripts.GameElements
             }
             else if (spinResultList.Value == null || spinResultList.Value.Length == 0)
             {
-                GenerateSpinListNew();        
+                GenerateSpinListNew();
             }
-        
         }
     }
+
 
     [Serializable]
     public class SpinData
     {
         public SpinResult spinResult;
-        [Range(1,100)]public int percentage;
+        [Range(1, 100)] public int percentage;
     }
 
     [Serializable]
@@ -197,7 +198,7 @@ namespace Game.Scripts.GameElements
     }
 
     [Serializable]
-    public enum SpinType 
+    public enum SpinType
     {
         Jackpot = 0,
         Wild = 1,
